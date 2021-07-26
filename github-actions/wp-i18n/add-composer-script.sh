e0f102cad1c790186edd10a0991dcddee1c97f08
Micah Wood <micah@wpscholar.com>
2021-07-26 15:57:57 -0400

    Ensure that the wp-i18n GitHub Action can add a script to Composer to run locally.

--- github-actions/wp-i18n/add-composer-script.sh
+++ github-actions/wp-i18n/add-composer-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VENDOR=$1
 PACKAGE=$2
-composer config scripts.i18n "vendor/bin/wp i18n make-pot . ./languages/$PACKAGE.pot --headers='{\"Report-Msgid-Bugs-To\":\"https://github.com/$VENDOR/$PACKAGE/issues\",\"POT-Creation-Date\":null}' --exclude=assets,storybook,tests"
+composer config scripts.i18n "wp i18n make-pot . ./languages/$PACKAGE.pot --headers='{\"Report-Msgid-Bugs-To\":\"https://github.com/$VENDOR/$PACKAGE/issues\",\"POT-Creation-Date\":null}' --exclude=assets,storybook,tests"
