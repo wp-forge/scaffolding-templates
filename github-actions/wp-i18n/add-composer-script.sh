a1cc063cb3980e99792113388bd6c1ad6d02e91b
Micah Wood <micah@wpscholar.com>
2021-07-27 12:41:03 -0400

    Clean up templates to make more robust

--- github-actions/wp-i18n/add-composer-script.sh
+++ github-actions/wp-i18n/add-composer-script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 VENDOR=$1
 PACKAGE=$2
-composer config scripts.i18n "wp i18n make-pot . ./languages/$PACKAGE.pot --headers='{\"Report-Msgid-Bugs-To\":\"https://github.com/$VENDOR/$PACKAGE/issues\",\"POT-Creation-Date\":null}' --exclude=assets,storybook,tests"
+PROJECT_ROOT=$3
+
+composer config -d $PROJECT_ROOT scripts.i18n "wp i18n make-pot . ./languages/$PACKAGE.pot --headers='{\"Report-Msgid-Bugs-To\":\"https://github.com/$VENDOR/$PACKAGE/issues\",\"POT-Creation-Date\":null}' --exclude=assets,storybook,tests"
